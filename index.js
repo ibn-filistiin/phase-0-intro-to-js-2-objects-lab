d4abc1c14c43e05f149837ac350d9ab26c38c025
adnan <106653813+ibn-filistiin@users.noreply.github.com>
2024-12-25 20:09:48 +0300

    object laab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// Create the employee object as a sample input
+const employee = {};
+
+// Non-destructive: returns a new object with updated key-value pair
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+// Destructive: updates the original object
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Non-destructive: deletes a key from a clone of the object
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Destructive: deletes a key from the original object
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+// Export functions to be used in the test file
+module.exports = {
+  updateEmployeeWithKeyAndValue,
+  destructivelyUpdateEmployeeWithKeyAndValue,
+  deleteFromEmployeeByKey,
+  destructivelyDeleteFromEmployeeByKey,
+};
+
